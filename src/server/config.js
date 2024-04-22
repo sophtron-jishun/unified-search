94ddb951f120ab795208f4b0c39104ddea2758f4
Wes Risenmay <wes.risenmay@mx.com>
2024-04-22 13:45:55 -0600

    added an env example file, and server config is now pulling from .env

--- src/server/config.js
+++ src/server/config.js
@@ -1,27 +1,37 @@
+require('dotenv').config({override: true});
+
 const processEnv = {};
 const envs = {...process.env, ...process.client_envs};
 Object.keys(envs).forEach((k) => {
   processEnv[k.toUpperCase()] = envs[k];
 });
-const config = {
-  LogLevel: 'debug',
-  Port: '8082',
-  Env: 'pre', //mocked
-  Component: 'unified-search',
-  Version: '',
-  MaxSearchResults: 50,
-  DataLoadIntervalSeconds: 30,
-  AnalyticsServiceEndpoint: 'https://analytics.universalconnectproject.org/api/',
-  AuthServiceEndpoint: 'https://login.universalconnectproject.org/api/',
-  DataBaseUrl: 'http://static.universalconnectproject.org.s3.us-west-2.amazonaws.com/search/',
-  S3Bucket: 'dev.universalconnectproject.org', // leave blank to use local server hardcoded preference
-  LocalDefaultProvider: 'sophtron', //default provider config when S3Bucket is not used.
-  AwsRegion: 'us-west-2',
-};;
 
-const arr = Object.keys(config);
-for (let i = 0; i < arr.length; i++) {
-  const key = arr[i];
-  config[key] = processEnv[key.toUpperCase()] || config[key];
-}
-module.exports = config;
+const nonSensitiveSharedConfig = {
+  Component: 'unified-search',
+};
+
+const keysToPullFromEnv = [
+  "LogLevel",
+  "Port",
+  "Env",
+  "Version",
+  "MaxSearchResults",
+  "DataLoadIntervalSeconds",
+  "AnalyticsServiceEndpoint",
+  "AuthServiceEndpoint",
+  "DataBaseUrl",
+  "S3Bucket",
+  "LocalDefaultProvider",
+  "AwsRegion",
+];
+
+const config = keysToPullFromEnv.reduce((acc, envKey) => {
+  return {
+    ...acc,
+    [envKey]: processEnv[envKey.toUpperCase()]
+  }
+}, {
+  ...nonSensitiveSharedConfig
+})
+
+module.exports = config
